4e7f9649914c7412ec2d6f53d90b10bc32a83bf7
directorfloo <folorunsho891@gmail.com>
2025-08-22 08:40:23 -0700

    Day5

---
+++ JAVA SCRIPT/Day5/ParkingSlot.js
@@ -0,0 +1,81 @@
+/*the clear a variable 
+Using the while loop to loop through the method
+Use booleen data type
+promt the user
+using switch case*/
+
+
+
+
+
+
+const prompt = require("prompt-sync")();
+
+let itemList = [];
+let mainMenu = true;
+
+while (mainMenu) {
+    let menuText = `
+    
+          WELCOME TO SEMICOLON GROCERY STORE  
+            
+            choose from the menu:
+
+
+		1. Park your car	
+		2. Remove your car
+		3. Show all available parking space
+		0. Exist
+    `;
+    console.log(menuText);
+    console.log("Enter choice 1, 2, 3 or 0 to quit: ");
+    let choice = Number(prompt("> "));
+
+
+    switch (choice) {
+        case 1:
+            console.log("1. Park your car: ");
+            let userInput = prompt("Enter item: ");  
+            addMyItem(userInput);
+            break;
+
+        case 2:
+            console.log("2. Remove your car");
+            let removeItem = prompt("Enter item to remove: ");
+            removeMyItem(removeItem);
+            break;
+
+        case 3:
+            console.log("3. Show all available parking space");
+            showItem();
+            break;
+
+        case 0:
+            mainMenu = false;
+            console.log("Good bye");
+            break;
+
+        default:
+            console.log("Invalid choice. Try again.");
+}
+}
+function addMyItem(addItem) {
+    itemList.push(addItem);
+    console.log(itemList);
+    return "list";
+}
+
+function removeMyItem(removeItem) {
+    let index = itemList.indexOf(removeItem);
+    if (index !== -1) {
+        itemList.splice(index, 1);
+}
+    console.log(itemList);
+    return "list";
+}
+
+function showItem() {
+    for (let index = 0; index < itemList.length; index++) {
+        console.log(itemList[index]);
+}
+}
